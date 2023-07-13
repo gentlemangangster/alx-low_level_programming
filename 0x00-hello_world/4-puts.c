78f5712fc297216c97aa675c4bafad38557f75e2
gentlemangangster <makuraterrance@gmail.com>
2023-07-13 03:33:21 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int mai(void)
+{
+	puts("\"Programminig is like building a multilingual puzzle");
+	return(0);
+}	
