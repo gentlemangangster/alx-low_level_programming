5a10db8f016086d0aab25e431d55e84634197cb2
gentlemangangster <makuraterrance@gmail.com>
2023-07-13 03:56:47 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main-Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int mai(void)
+int main(void)
 {
 	puts("\"Programminig is like building a multilingual puzzle");
 	return(0);
